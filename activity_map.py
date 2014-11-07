e99784536aea06e5233d7f80c271e56830537dfd
Michal Sadowski <michal.sadowski@rawlplug.co>
2014-11-07 12:37:04 +0100

    PEP8 corrections in activity_map.py

--- activity_map.py
+++ activity_map.py
@@ -1,7 +1,7 @@
 #!/usr/env/python -tt
 # -*- encoding=utf8 -*-
 #
-#   Copyright 2009-2012 Michal Sadowski (sq6jnx at hamradio dot pl)
+#   Copyright 2009-2014 Michal Sadowski (sq6jnx at hamradio dot pl)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -16,36 +16,33 @@
 #   limitations under the License.
 #
 
-import urllib
-import json
+from config import activity_map as config
 import base64
 import debug
-from config import activity_map as config
+import json
+import urllib
 
-
-#def my_import(name):
-#    mod = __import__(name)
-#    components = name.split('.')
-#    for comp in components[1:]:
-#        mod = getattr(mod, comp)
-#    return mod
 
 def getData(l):
     """This module does NOT return any data! It is here just to say "hello" to
     map utility!"""
 
-    data = {"data":"", "needCTCSS":False, "debug":None, "allOK":True}
+    data = {"data": "",
+            "needCTCSS": False,
+            "debug": None,
+            "allOK": True,
+            }
 
-    dump = json.dumps(config.data, separators=(',',':'))
+    dump = json.dumps(config.data, separators=(',', ':'))
     b64data = base64.urlsafe_b64encode(dump)
 
-    if urllib.urlopen(config.service_url+b64data).read()=='OK':
+    if urllib.urlopen(config.service_url + b64data).read() == 'OK':
         debug.log("ACT_MAP", "Message sent, status OK")
     else:
-        debug.log("ACT_MAP", "Non-OK response from "+\
-                config.service_url+b64data, 6)
+        msg = "Non-OK response from %s"
+        debug.log("ACT_MAP", msg % config.service_url + b64data, 6)
 
     return data
 
-if __name__=='__main__':
+if __name__ == '__main__':
     getData('pl_google')
