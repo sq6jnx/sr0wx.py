6bbe7809a4ec4c2e705306d4b578407769527b8f
Michal Sadowski <sq6jnx@hamradio.pl>
2014-11-09 21:08:09 +0100

    PEP8 corrections in hscr_laviny.py

--- hscr_laviny.py
+++ hscr_laviny.py
@@ -1,7 +1,7 @@
 #!/usr/env/python -tt
 # -*- encoding=utf8 -*-
 #
-#   Copyright 2009-2011 Michal Sadowski (sq6jnx at hamradio dot pl)
+#   Copyright 2009-2011, 2014 Michal Sadowski (sq6jnx at hamradio dot pl)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -21,98 +21,109 @@ import urllib
 
 from config import hscr_laviny as config
 import datetime
-lang=None
+lang = None
+
 
 def downloadFile(url):
-	webFile = urllib.urlopen(url)
-	return webFile.read()
+    webFile = urllib.urlopen(url)
+    return webFile.read()
+
 
 def my_import(name):
-	mod = __import__(name)
-	components = name.split('.')
-	for comp in components[1:]:
-		mod = getattr(mod, comp)
-	return mod
+    mod = __import__(name)
+    components = name.split('.')
+    for comp in components[1:]:
+        mod = getattr(mod, comp)
+    return mod
 
-def last(list):
-	if len(list)==0:
-		return None
-	else:
-		return list[-1]
+
+def last(l):
+    if len(l) == 0:
+        return None
+    else:
+        return l[-1]
+
 
 def getAwareness(region):
-	# This is the main page of avalanche awareness system of HS CR:
-	url = "http://www.horskasluzba.cz/laviny/"
+    # This is the main page of avalanche awareness system of HS CR:
+    url = "http://www.horskasluzba.cz/laviny/"
 
-	# We'll have to find link to the most actual info for the specified region:
-	r = u'id\=(\d{4,}).{10,}Lavinov.{5,}'+region # UGLY, but \" doesn't work...
-	_id = re.compile(r)
-	
-	for line in downloadFile(url).split('\n'):
-		id = _id.findall(line)
-		if id != []:
-			id = int(id[0])
-			break
+    # We'll have to find link to the most actual info for the specified region:
+    r = u'id\=(\d{4,}).{10,}Lavinov.{5,}' + region
+    _id = re.compile(r)
 
-	# Now we have the proper URL:
-	url = "http://www.horskasluzba.cz/laviny/showlavina.php?id=%d"%(id,)
+    for line in downloadFile(url).split('\n'):
+        id = _id.findall(line)
+        if id != []:
+            id = int(id[0])
+            break
 
-	# We'll now parse html looking for these:
-	_level      = re.compile('/gfx/stupen(\d).gif')
-	_tendention = re.compile('/gfx/tendence(\d).gif')
-	_exposition = re.compile('/gfx/expozice(\d).gif')
-	_date       = re.compile('Datum:\ (\d{1,2})\.\ (\d{1,2})\.\ (\d{4})')
+    # Now we have the proper URL:
+    url = "http://www.horskasluzba.cz/laviny/showlavina.php?id=%d" % (id,)
 
-	level, tendention, exposition, date = False,False,False,False
+    # We'll now parse html looking for these:
+    _level = re.compile('/gfx/stupen(\d).gif')
+    _tendention = re.compile('/gfx/tendence(\d).gif')
+    _exposition = re.compile('/gfx/expozice(\d).gif')
+    _date = re.compile('Datum:\ (\d{1,2})\.\ (\d{1,2})\.\ (\d{4})')
 
-	for line in downloadFile(url).split('\n'):
-		line = unicode(line, "cp1250")
-		level      = level      or last(_level.findall(line))
-		tendention = tendention or last(_tendention.findall(line))
-		exposition = exposition or last(_exposition.findall(line))
-		if _date.findall(line)!=[]:
-			dt       = _date.findall(line)[0]
-			now      = datetime.datetime.now()
-			delta    = datetime.timedelta(hours=24)
-			infoDT = datetime.datetime(int(dt[2]), int(dt[1]), int(dt[0]), 12, 00)
-			if infoDT+datetime.timedelta(hours=48)<now:
-				isActual = None
-			elif infoDT+datetime.timedelta(hours=24)<now:
-				isActual = False
-			else:
-				isActual=True
+    level, tendention, exposition, date = False, False, False, False
 
-	return (int(level), int(tendention), int(exposition), isActual, infoDT)
+    for line in downloadFile(url).split('\n'):
+        line = unicode(line, "cp1250")
+        level = level or last(_level.findall(line))
+        tendention = tendention or last(_tendention.findall(line))
+        exposition = exposition or last(_exposition.findall(line))
+        if _date.findall(line) != []:
+            dt = _date.findall(line)[0]
+            now = datetime.datetime.now()
+            infoDT = datetime.datetime(int(dt[2]), int(dt[1]), int(dt[0]), 12, 00)
+            if infoDT + datetime.timedelta(hours=48) < now:
+                isActual = None
+            elif infoDT + datetime.timedelta(hours=24) < now:
+                isActual = False
+            else:
+                isActual = True
+
+    return (int(level), int(tendention), int(exposition), isActual, infoDT)
+
 
 def getData(l):
     global lang
     lang = my_import(l+"."+l)
 
-    data = {"data":"", "needCTCSS":False, "debug":None, "allOK":True,
-            "source":""} # given by welcome message
+    data = {"data": "",
+            "needCTCSS": False,
+            "debug": None,
+            "allOK": True,
+            "source": ""}
 
-    level, tendention, exposition, isActual, infoDT = getAwareness(config.region)
+    level, tendention, _, isActual, infoDT = getAwareness(config.region)
 
-    if isActual == None:
-	    return data
+    if isActual is None:
+        return data
     else:
-        data["needCTCSS"]=True
+        data["needCTCSS"] = True
 
-    data["data"] = " ".join( (data["data"], lang.hscr_region[config.region[0]], lang.avalancheLevel[level]) )
+    data["data"] = " ".join((data["data"],
+                             lang.hscr_region[config.region[0]],
+                             lang.avalancheLevel[level]))
 
-    if config.giveTendention==1:
-        data["data"] = " ".join( (data["data"], lang.hscr_tendention[tendention]) )
+    if config.giveTendention == 1:
+        data["data"] = " ".join((data["data"],
+                                 lang.hscr_tendention[tendention]))
 
-    if isActual==False:
-        data["data"] = " ".join( (data["data"], lang.info_at, lang.readISODate(infoDT.isoformat(' '))) )
+    if not isActual:
+        data["data"] = " ".join((data["data"],
+                                 lang.info_at,
+                                 lang.readISODate(infoDT.isoformat(' '))))
 
-    # Profile i szczególnie niebezpieczne wystawy niezaimplementowane.
+    data["data"] = lang.removeDiacritics(lang.hscr_welcome
+                                         + ' _ '
+                                         + data["data"])
 
-    data["data"] = lang.removeDiacritics(lang.hscr_welcome+' _ '+data["data"])
-
-    return data	
+    return data
 
 if __name__ == '__main__':
     lang = 'pl'
     print getData('pl')
-
