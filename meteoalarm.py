68a62469724108e9e43458b07ab19ad31935bf04
Michal Sadowski <sq6jnx@hamradio.pl>
2014-11-09 17:42:43 +0100

    PEP8 corrections in meteoalarm.py

--- meteoalarm.py
+++ meteoalarm.py
@@ -1,7 +1,7 @@
 #!/usr/env/python -tt
 # -*- encoding=utf8 -*-
 #
-#   Copyright 2009-2011 Michal Sadowski (sq6jnx at hamradio dot pl)
+#   Copyright 2009-2011, 2014 Michal Sadowski (sq6jnx at hamradio dot pl)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -20,11 +20,13 @@ import re
 import urllib
 
 from config import meteoalarm as config
-lang=None
+lang = None
+
 
 def downloadFile(url):
     webFile = urllib.urlopen(url)
     return webFile.read()
+
 
 def my_import(name):
     mod = __import__(name)
@@ -33,48 +35,63 @@ def my_import(name):
         mod = getattr(mod, comp)
     return mod
 
+
 def getData(l):
     global lang
-    lang = my_import(l+"."+l)
+    lang = my_import(l + "." + l)
 
-    data = {"data":"", "needCTCSS":False, "debug":None, "allOK":True,
-            "source":"meteoalarm_eu"}
-    
-    today=tomorrow=""
-    if config.showToday == True:
-        today    = getAwareness(config.region, tomorrow=False)
-    if config.showTomorrow == True:
+    data = {"data": "",
+            "needCTCSS": False,
+            "debug": None,
+            "allOK": True,
+            "source": "meteoalarm_eu"}
+
+    today = tomorrow = ""
+    if config.showToday:
+        today = getAwareness(config.region, tomorrow=False)
+    if config.showTomorrow:
         tomorrow = getAwareness(config.region, tomorrow=True)
 
-    if today== "" and tomorrow== "":
-        #data["data"] = " ".join( (lang.meteoalarmNoAwareness, lang.meteoalarmRegions[config.region]) )
-        pass # silence is golden
-    elif today!= "" and tomorrow=="":
-        data["data"] =  " ".join( (lang.meteoalarmAwareness, lang.meteoalarmRegions[config.region], lang.today, today) )
+    if today == "" and tomorrow == "":
+        pass
+    elif today != "" and tomorrow == "":
+        data["data"] = " ".join((lang.meteoalarmAwareness,
+                                 lang.meteoalarmRegions[config.region],
+                                 lang.today,
+                                 today
+                                 ))
         data["needCTCSS"] = True
-    elif today== "" and tomorrow!="":
-        data["data"] =  " ".join( (lang.meteoalarmAwareness, lang.meteoalarmRegions[config.region], lang.tomorrow, tomorrow) )
-        data["needCTCSS"]= True
+    elif today == "" and tomorrow != "":
+        data["data"] = " ".join((lang.meteoalarmAwareness,
+                                 lang.meteoalarmRegions[config.region],
+                                 lang.tomorrow,
+                                 tomorrow
+                                 ))
+        data["needCTCSS"] = True
     else:
-        data["data"] = " ".join( (lang.meteoalarmAwareness, lang.meteoalarmRegions[config.region], lang.today, today, lang.tomorrow,tomorrow) )
-        data["needCTCSS"]= True 
+        data["data"] = " ".join((lang.meteoalarmAwareness,
+                                 lang.meteoalarmRegions[config.region],
+                                 lang.today,
+                                 today,
+                                 lang.tomorrow,
+                                 tomorrow
+                                 ))
+        data["needCTCSS"] = True
 
     return data
 
+
 def getAwareness(region, tomorrow=False):
-# tommorow = False -- awareness for today
-# tommorow = True  -- awareness for tommorow
-    r =   re.compile('pictures/aw(\d[01]?)([0234]).jpg')
+    r = re.compile('pictures/aw(\d[01]?)([0234]).jpg')
     url = "http://www.meteoalarm.eu/index3.php?area=%s&day=%s&lang=EN"\
-        %(str(region),str(int(tomorrow)))
-
-
-    a = ""
+        % (str(region), str(int(tomorrow)))
 
     for line in downloadFile(url).split('\n'):
         f = r.findall(line)
-        if len(f) is not 0 and int(f[0][0])!=0:
-            a = " ".join( (a, lang.meteoalarmAwarenesses[int(f[0][0])],\
-                lang.meteoalarmAwarenessLevel, lang.meteoalarmAwarenessLvl[int(f[0][1])]) )
+        if len(f) is not 0 and int(f[0][0]) != 0:
+            a = " ".join((lang.meteoalarmAwarenesses[int(f[0][0])],
+                          lang.meteoalarmAwarenessLevel,
+                          lang.meteoalarmAwarenessLvl[int(f[0][1])],
+                          ))
 
     return a
