dadb3cf5f98aadf1fe1ac9542351dd8da11aa87a
Michal Sadowski <sq6jnx@hamradio.pl>
2014-11-09 21:08:09 +0100

    PEP8 corrections in gopr_lawiny.py

--- gopr_lawiny.py
+++ gopr_lawiny.py
@@ -1,7 +1,7 @@
 #!/usr/env/python -tt
 # -*- encoding=utf8 -*-
 #
-#   Copyright 2009-2011 Michal Sadowski (sq6jnx at hamradio dot pl)
+#   Copyright 2009-2014 Michal Sadowski (sq6jnx at hamradio dot pl)
 #
 #   Licensed under the Apache License, Version 2.0 (the "License");
 #   you may not use this file except in compliance with the License.
@@ -20,12 +20,14 @@ import re
 import urllib
 
 from config import gopr_lawiny as config
-import datetime
-lang=None
+
+lang = None
+
 
 def downloadFile(url):
     webFile = urllib.urlopen(url)
     return webFile.read()
+
 
 def my_import(name):
     mod = __import__(name)
@@ -34,69 +36,62 @@ def my_import(name):
         mod = getattr(mod, comp)
     return mod
 
-def last(list):
-    if len(list)==0:
+
+def last(l):
+    if len(l) == 0:
         return None
     else:
-        return list[-1]
+        return l[-1]
+
 
 def pobierzOstrzezenia(region):
-    url = "http://www.gopr.pl/avalanche/pl/%d"%region
+    url = "http://www.gopr.pl/avalanche/pl/%d" % region
 
-    aktualny = True
-    infoDT = None
-
-    _date      = re.compile('(\d{4})-(\d{2})-(\d{2})\ (\d{2}):(\d{2}):(\d{2})')
-    _brak      = re.compile('w momencie zaistnienia adekwatnych')
-    _stopien   = re.compile('img/([1-5])\.gif')
+    _date = re.compile('(\d{4})-(\d{2})-(\d{2})\ (\d{2}):(\d{2}):(\d{2})')
+    _brak = re.compile('w momencie zaistnienia adekwatnych')
+    _stopien = re.compile('img/([1-5])\.gif')
     _tendencja = re.compile("img/lawina/strzalka(\d)\.gif")
-    _wystawa   = re.compile("img/lawina/pikto/roza(\d)\.gif")
+    _wystawa = re.compile("img/lawina/pikto/roza(\d)\.gif")
 
-    brak,stopien, tendencja, wystawa = False,False,False,False
+    stopien, tendencja, wystawa = False
     line = downloadFile(url)
-    if _brak.findall(line)!=[]:
-	    return (-1,-1,-1)
+    if _brak.findall(line) != []:
+        return (-1, -1, -1)
     else:
-        stopien   = stopien   or _stopien.findall(line)[0]
+        stopien = stopien or stopien.findall(line)[0]
         tendencja = tendencja or _tendencja.findall(line)[0]
-        wystawa   = wystawa   or _wystawa.findall(line)[0]
-    if _date.findall(line)!=[]:
-        dt      = _date.findall(line)[0]
-        now     = datetime.datetime.now()
-        delta   = datetime.timedelta(hours=24)
-        infoDT  = datetime.datetime(int(dt[0]), int(dt[1]), int(dt[2]),
-                int(dt[3]), int(dt[4]), int(dt[5]))
-        if infoDT+delta<now:
-                 aktualny = False
-
+        wystawa = wystawa or _wystawa.findall(line)[0]
     return (int(stopien), int(tendencja), int(wystawa))
+
 
 def getData(l):
     global lang
-    lang = my_import(l+"."+l)
+    lang = my_import(l + "." + l)
 
-    data = {"data":"", "needCTCSS":False, "debug":None, "allOK":True,
-            "source":""} # given by welcome message
+    data = {"data": "",
+            "needCTCSS": False,
+            "debug": None,
+            "allOK": True,
+            "source": ""}
 
-    stopien, tendencja, wystawa = pobierzOstrzezenia(config.region)
+    stopien, tendencja = pobierzOstrzezenia(config.region)
 
     if stopien == -1:
         return ""
 
-    data["needCTCSS"]=True
+    data["needCTCSS"] = True
+    data["data"] = " ".join((data["data"],
+                            lang.gopr_region[config.region],
+                            lang.avalancheLevel[stopien]))
 
-    data["data"] = " ".join( (data["data"], lang.gopr_region[config.region], lang.avalancheLevel[stopien]) )
-
-    if config.podajTendencje==1:
-        data["data"] = " ".join( (lang.gopr_welcome, data["data"], lang.gopr_tendention[tendencja]) )
-
-    # Profile i szczególnie niebezpieczne wystawy niezaimplementowane.
+    if config.podajTendencje == 1:
+        data["data"] = " ".join((lang.gopr_welcome, data["data"],
+                                 lang.gopr_tendention[tendencja]))
 
     data["data"] = lang.removeDiacritics(data["data"])
 
-    return data	
+    return data
 
 if __name__ == '__main__':
     lang = 'pl'
     print getData(lang)
-
