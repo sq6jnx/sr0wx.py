6111bdb68448e2bba73a29091efd68f2ec772e82
Michał Sadowski <sq6jnx@hamradio.pl>
2014-11-25 17:54:20 +0100

    Changed way how license is shown

--- sr0wx.py
+++ sr0wx.py
@@ -1,20 +1,27 @@
 #!/usr/bin/python -tt
 # -*- coding: utf-8 -*-
-#
-#   Copyright 2009-2011, 2014 Michal Sadowski (sq6jnx at hamradio dot pl)
-#
-#   Licensed under the Apache License, Version 2.0 (the "License");
-#   you may not use this file except in compliance with the License.
-#   You may obtain a copy of the License at
-#
-#       http://www.apache.org/licenses/LICENSE-2.0
-#
-#   Unless required by applicable law or agreed to in writing, software
-#   distributed under the License is distributed on an "AS IS" BASIS,
-#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-#   See the License for the specific language governing permissions and
-#   limitations under the License.
-#
+
+LICENSE = """
+
+Copyright 2009-2014 Michal Sadowski (sq6jnx at hamradio dot pl)
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+
+-----------------------------------------------------------
+
+You can find full list of contributors on github.com/sq6jnx/sr0wx.py
+
+"""
 
 #
 #
@@ -133,21 +140,7 @@ if config is None:
 logger = setup_logging(config)
 
 logger.info("sr0wx.py started")
-logger.info('''
-Copyright 2009-2014 Michal Sadowski (sq6jnx at hamradio dot pl)
-
-Licensed under the Apache License, Version 2.0 (the \"License\");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an \"AS IS\" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.''')
-logger.info("sr0wx.py started")
+logger.info(LICENSE)
 
 
 if len(args) > 0:
