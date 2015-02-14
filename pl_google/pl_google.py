92057cd5a0cd7d9aa397ca812e85f7b830010564
Michał Sadowski <sq6jnx@hamradio.pl>
2015-02-14 11:44:46 +0100

    PLGoogle is now a class (#20)

--- pl_google/pl_google.py
+++ pl_google/pl_google.py
@@ -65,153 +65,163 @@ def _(text):
     return text.replace(' ', '_')
 
 
-@remove_accents
-def read_number(value, units=None):
-    """Converts numbers to text."""
-    if units is None:
-        retval = pyliczba.lslownie(abs(value))
-    else:
-        retval = pyliczba.cosslownie(abs(value), units)
-
-    if retval.startswith(u("jeden tysiąc")):
-        retval = retval.replace(u("jeden tysiąc"), u("tysiąc"))
-    if value < 0:
-        retval = " ".join(("minus", retval))
-    return retval
-
-@remove_accents
-def read_pressure(value):
-    hPa = ["hektopaskal", "hektopaskale", "hektopaskali"]
-    return read_number(value, hPa)
-
-@remove_accents
-def read_percent(value):
-    percent = ["procent", "procent", "procent"]
-    return read_number(value, percent)
-
-@remove_accents
-def read_temperature(value):
-    C = [_(u("stopień Celsjusza")), _("stopnie Celsjusza"), _("stopni Celsjusza")]
-    return read_number(value, C)
-
-@remove_accents
-def read_speed(no, unit='mps'):
-    units = {
-        'mps': [_(u("metr na sekundę")), _(u("metry na sekundę")),
-                _(u("metrów na sekundę"))],
-        'kmph': [_(u("kilometr na godzinę")), _(u("kilometry na godzinę")),
-                 _(u("kilometrów na godzinę"))]
-    }
-    return read_number(no, units[unit])
-
-@remove_accents
-def read_degrees(value):
-    deg = [u("stopień"), u("stopnie"), u("stopni")]
-    return read_number(value, deg)
-
-
-@remove_accents
-def read_direction(value, short=False):
-    directions = {
-        "N": (u("północno"),   u("północny")),
-        "E": (u("wschodnio"),  u("wschodni")),
-        "W": (u("zachodnio"),  u("zachodni")),
-        "S": (u("południowo"), u("południowy")),
-    }
-    if short:
-        value = value[-2:]
-    return '-'.join([directions[d][0 if i < 0 else 1]
-                     for i, d in enumerate(value, -len(value)+1)])
-
-
-@remove_accents
-def read_datetime(value, out_fmt, in_fmt=None):
-
-    if type(value) != datetime.datetime and in_fmt is not None:
-        value = datetime.datetime.strptime(value, in_fmt)
-    elif type(value) == datetime.datetime:
+class SR0WXLanguage(object):
+    def __init__(self):
+        """Nothing here for now."""
         pass
-    else:
-        raise TypeError('Either datetime must be supplied or both '
-                        'value and in_fmt')
-
-    MONTHS = [u(""),
-              u("stycznia"), u("lutego"), u("marca"), u("kwietnia"), u("maja"),
-              u("czerwca"), u("lipca"), u("sierpnia"), u("września"),
-              u("października"), u("listopada"), u("grudnia"),
-    ]
-
-    DAYS_N0 = [u(""), u(""), u("dwudziestego"), u("trzydziestego"),]
-    DAYS_N = [u(""),
-              u("pierwszego"), u("drugiego"), u("trzeciego"), u("czwartego"),
-              u("piątego"), u("szóstego"), u("siódmego"), u("ósmego"),
-              u("dziewiątego"), u("dziesiątego"), u("jedenastego"),
-              u("dwunastego"), u("trzynastego"), u("czternastego"),
-              u("piętnastego"), u("szesnastego"), u("siedemnastego"),
-              u("osiemnastego"), u("dziewiętnastego"),
-    ]
-    HOURS = [u("zero"), u("pierwsza"), u("druga"), u("trzecia"), u("czwarta"),
-             u("piąta"), u("szósta"), u("siódma"), u("ósma"), u("dziewiąta"),
-             u("dziesiąta"), u("jedenasta"), u("dwunasta"), u("trzynasta"),
-             u("czternasta"), u("piętnasta"), u("szesnasta"),
-             u("siedemnasta"), u("osiemnasta"), u("dziewiętnasta"),
-             u("dwudziesta"),
-    ]
 
 
-    _, tm_mon, tm_mday, tm_hour, tm_min, _, _, _, _ = value.timetuple()
-    retval = []
-    for word in out_fmt.split(" "):
-        if word == '%d':  # Day of the month
-            if tm_mday <= 20:
-                retval.append(DAYS_N[tm_mday])
-            else:
-                retval.append(DAYS_N0[tm_mday //10])
-                retval.append(DAYS_N[tm_mday % 10])
-        elif word == '%B':  # Month as locale’s full name 
-            retval.append(MONTHS[tm_mon])
-        elif word == '%H':  # Hour (24-hour clock) as a decimal number
-            if tm_hour <= 20:
-                retval.append(HOURS[tm_hour])
-            elif tm_hour > 20:
-                retval.append(HOURS[20])
-                retval.append(HOURS[tm_hour - 20])
-        elif word == '%M':  # Minute as a decimal number
-            if tm_min == 0:
-                retval.append(u('zero-zero'))
-            else:
-                retval.append(read_number(tm_min))
-        elif word.startswith('%'):
-            raise ValueError("Token %s' is not supported!", word)
+class PLGoogle(SR0WXLanguage):
+    def __init__(self):
+        pass
+
+    @remove_accents
+    def read_number(self, value, units=None):
+        """Converts numbers to text."""
+        if units is None:
+            retval = pyliczba.lslownie(abs(value))
         else:
-            retval.append(word)
-    return ' '.join((w for w in retval if w != ''))
+            retval = pyliczba.cosslownie(abs(value), units)
 
-@remove_accents
-def read_callsign(value):
-    # literowanie polskie wg. "Krótkofalarstwo i radiokomunikacja - poradnik",
-    # Łukasz Komsta SQ8QED, Wydawnictwa Komunikacji i Łączności Warszawa, 2001,
-    # str. 130
-    LETTERS = {
-        'a': u('adam'), 'b': u('barbara'), 'c': u('celina'), 'd': u('dorota'),
-        'e': u('edward'), 'f': u('franciszek'), 'g': u('gustaw'),
-        'h': u('henryk'), 'i': u('irena'), 'j': u('józef'), 'k': u('karol'),
-        'l': u('ludwik'), 'm': u('marek'), 'n': u('natalia'), 'o': u('olga'),
-        'p': u('paweł'), 'q': u('quebec'), 'r': u('roman'), 's': u('stefan'),
-        't': u('tadeusz'), 'u': u('urszula'), 'v': u('violetta'),
-        'w': u('wacław'), 'x': u('xawery'), 'y': u('ypsilon'), 'z': u('zygmunt'),
-        '/': u('łamane'),
-    }
-    retval = []
-    for char in value.lower():
-        try:
-            retval.append(LETTERS[char])
-        except KeyError:
+        if retval.startswith(u("jeden tysiąc")):
+            retval = retval.replace(u("jeden tysiąc"), u("tysiąc"))
+        if value < 0:
+            retval = " ".join(("minus", retval))
+        return retval
+
+    @remove_accents
+    def read_pressure(self, value):
+        hPa = ["hektopaskal", "hektopaskale", "hektopaskali"]
+        return self.read_number(value, hPa)
+
+    @remove_accents
+    def read_percent(self, value):
+        percent = ["procent", "procent", "procent"]
+        return self.read_number(value, percent)
+
+    @remove_accents
+    def read_temperature(self, value):
+        C = [_(u("stopień Celsjusza")), _("stopnie Celsjusza"), _("stopni Celsjusza")]
+        return read_number(value, C)
+
+    @remove_accents
+    def read_speed(self, no, unit='mps'):
+        units = {
+            'mps': [_(u("metr na sekundę")), _(u("metry na sekundę")),
+                    _(u("metrów na sekundę"))],
+            'kmph': [_(u("kilometr na godzinę")), _(u("kilometry na godzinę")),
+                     _(u("kilometrów na godzinę"))]
+        }
+        return read_number(no, units[unit])
+
+    @remove_accents
+    def read_degrees(self, value):
+        deg = [u("stopień"), u("stopnie"), u("stopni")]
+        return read_number(value, deg)
+
+
+    @remove_accents
+    def read_direction(self, value, short=False):
+        directions = {
+            "N": (u("północno"),   u("północny")),
+            "E": (u("wschodnio"),  u("wschodni")),
+            "W": (u("zachodnio"),  u("zachodni")),
+            "S": (u("południowo"), u("południowy")),
+        }
+        if short:
+            value = value[-2:]
+        return '-'.join([directions[d][0 if i < 0 else 1]
+                         for i, d in enumerate(value, -len(value)+1)])
+
+
+    @remove_accents
+    def read_datetime(self, value, out_fmt, in_fmt=None):
+
+        if type(value) != datetime.datetime and in_fmt is not None:
+            value = datetime.datetime.strptime(value, in_fmt)
+        elif type(value) == datetime.datetime:
+            pass
+        else:
+            raise TypeError('Either datetime must be supplied or both '
+                            'value and in_fmt')
+
+        MONTHS = [u(""),
+                  u("stycznia"), u("lutego"), u("marca"), u("kwietnia"), u("maja"),
+                  u("czerwca"), u("lipca"), u("sierpnia"), u("września"),
+                  u("października"), u("listopada"), u("grudnia"),
+        ]
+
+        DAYS_N0 = [u(""), u(""), u("dwudziestego"), u("trzydziestego"),]
+        DAYS_N = [u(""),
+                  u("pierwszego"), u("drugiego"), u("trzeciego"), u("czwartego"),
+                  u("piątego"), u("szóstego"), u("siódmego"), u("ósmego"),
+                  u("dziewiątego"), u("dziesiątego"), u("jedenastego"),
+                  u("dwunastego"), u("trzynastego"), u("czternastego"),
+                  u("piętnastego"), u("szesnastego"), u("siedemnastego"),
+                  u("osiemnastego"), u("dziewiętnastego"),
+        ]
+        HOURS = [u("zero"), u("pierwsza"), u("druga"), u("trzecia"), u("czwarta"),
+                 u("piąta"), u("szósta"), u("siódma"), u("ósma"), u("dziewiąta"),
+                 u("dziesiąta"), u("jedenasta"), u("dwunasta"), u("trzynasta"),
+                 u("czternasta"), u("piętnasta"), u("szesnasta"),
+                 u("siedemnasta"), u("osiemnasta"), u("dziewiętnasta"),
+                 u("dwudziesta"),
+        ]
+
+
+        _, tm_mon, tm_mday, tm_hour, tm_min, _, _, _, _ = value.timetuple()
+        retval = []
+        for word in out_fmt.split(" "):
+            if word == '%d':  # Day of the month
+                if tm_mday <= 20:
+                    retval.append(DAYS_N[tm_mday])
+                else:
+                    retval.append(DAYS_N0[tm_mday //10])
+                    retval.append(DAYS_N[tm_mday % 10])
+            elif word == '%B':  # Month as locale’s full name
+                retval.append(MONTHS[tm_mon])
+            elif word == '%H':  # Hour (24-hour clock) as a decimal number
+                if tm_hour <= 20:
+                    retval.append(HOURS[tm_hour])
+                elif tm_hour > 20:
+                    retval.append(HOURS[20])
+                    retval.append(HOURS[tm_hour - 20])
+            elif word == '%M':  # Minute as a decimal number
+                if tm_min == 0:
+                    retval.append(u('zero-zero'))
+                else:
+                    retval.append(read_number(tm_min))
+            elif word.startswith('%'):
+                raise ValueError("Token %s' is not supported!", word)
+            else:
+                retval.append(word)
+        return ' '.join((w for w in retval if w != ''))
+
+    @remove_accents
+    def read_callsign(self, value):
+        # literowanie polskie wg. "Krótkofalarstwo i radiokomunikacja - poradnik",
+        # Łukasz Komsta SQ8QED, Wydawnictwa Komunikacji i Łączności Warszawa, 2001,
+        # str. 130
+        LETTERS = {
+            'a': u('adam'), 'b': u('barbara'), 'c': u('celina'), 'd': u('dorota'),
+            'e': u('edward'), 'f': u('franciszek'), 'g': u('gustaw'),
+            'h': u('henryk'), 'i': u('irena'), 'j': u('józef'), 'k': u('karol'),
+            'l': u('ludwik'), 'm': u('marek'), 'n': u('natalia'), 'o': u('olga'),
+            'p': u('paweł'), 'q': u('quebec'), 'r': u('roman'), 's': u('stefan'),
+            't': u('tadeusz'), 'u': u('urszula'), 'v': u('violetta'),
+            'w': u('wacław'), 'x': u('xawery'), 'y': u('ypsilon'), 'z': u('zygmunt'),
+            '/': u('łamane'),
+        }
+        retval = []
+        for char in value.lower():
             try:
-                retval.append(read_number(int(char)))
-            except ValueError:
-                raise ValueError("\"%s\" is not a element of callsign", char)
-    return ' '.join(retval)
+                retval.append(LETTERS[char])
+            except KeyError:
+                try:
+                    retval.append(read_number(int(char)))
+                except ValueError:
+                    raise ValueError("\"%s\" is not a element of callsign", char)
+        return ' '.join(retval)
 
 
 # ##########################################
@@ -276,3 +286,16 @@ wwo_weather_codes = {
 
 # to be removed from code
 source = 'zrodlo'
+
+pl = PLGoogle()
+
+read_number = pl.read_number
+read_pressure = pl.read_pressure
+read_percent = pl.read_percent
+read_temperature = pl.read_temperature
+read_speed = pl.read_speed
+read_degrees = pl.read_degrees
+read_direction = pl.read_direction
+read_datetime = pl.read_datetime
+read_callsign = pl.read_callsign
+
