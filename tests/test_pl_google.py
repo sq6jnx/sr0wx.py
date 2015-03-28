12bc0de8bc1f7376cb1e330fb1357a44c8a92801
Michał Sadowski <sq6jnx@hamradio.pl>
2015-03-28 15:59:16 +0100

    Simplified PLGoogle test methods (#20)

--- tests/test_pl_google.py
+++ tests/test_pl_google.py
@@ -4,99 +4,175 @@ import unittest
 import pl_google.pl_google as pl_google
 import datetime
 
-class TestKwotaSlownieFunctions(unittest.TestCase):
-    """Klasa do testów jednostkowych"""
 
+class TestPLGoogle(unittest.TestCase):
     def setUp(self):
         self.lang = pl_google.PLGoogle()
-        pass
 
     def test_read_number(self):
-        self.assertEqual(self.lang.read_number(5), "piec")
-        self.assertEqual(self.lang.read_number(42), "czterdziesci dwa")
-        self.assertEqual(self.lang.read_number(666), "szescset szescdziesiat szesc")
-        self.assertEqual(self.lang.read_number(1024), "tysiac dwadziescia cztery")
+        test_cases = {
+            5: "piec",
+            42: "czterdziesci dwa",
+            666: "szescset szescdziesiat szesc",
+            1024: "tysiac dwadziescia cztery",
+        }
+
+        for value, expected in test_cases.items():
+            self.assertEqual(expected, self.lang.read_number(value))
 
     def test_read_pressure(self):
-        self.assertEqual(self.lang.read_pressure(1), "jeden hektopaskal")
-        self.assertEqual(self.lang.read_pressure(999), "dziewiecset dziewiecdziesiat dziewiec hektopaskali")
-        self.assertEqual(self.lang.read_pressure(1002), "tysiac dwa hektopaskale")
+        test_cases = {
+            1: "jeden hektopaskal",
+            999: "dziewiecset dziewiecdziesiat dziewiec hektopaskali",
+            1002: "tysiac dwa hektopaskale",
+        }
+
+        for value, expected in test_cases.items():
+            self.assertEqual(expected, self.lang.read_pressure(value))
 
     def test_read_percent(self):
-        self.assertEqual(self.lang.read_percent(1), "jeden procent")
-        self.assertEqual(self.lang.read_percent(2), "dwa procent")
-        self.assertEqual(self.lang.read_percent(5), "piec procent")
+        test_cases = {
+            1: "jeden procent",
+            2: "dwa procent",
+            5: "piec procent",
+        }
+
+        for value, expected in test_cases.items():
+            self.assertEqual(expected, self.lang.read_percent(value))
 
     def test_read_temperature(self):
-        self.assertEqual(self.lang.read_temperature(-20), "minus dwadziescia stopni_celsjusza")
-        self.assertEqual(self.lang.read_temperature(-2), "minus dwa stopnie_celsjusza")
-        self.assertEqual(self.lang.read_temperature(-1), "minus jeden stopien_celsjusza")
-        self.assertEqual(self.lang.read_temperature(0), "zero stopni_celsjusza")
-        self.assertEqual(self.lang.read_temperature(1), "jeden stopien_celsjusza")
-        self.assertEqual(self.lang.read_temperature(2), "dwa stopnie_celsjusza")
-        self.assertEqual(self.lang.read_temperature(20), "dwadziescia stopni_celsjusza")
+        test_cases = {
+            -20: "minus dwadziescia stopni_celsjusza",
+            -2: "minus dwa stopnie_celsjusza",
+            -1: "minus jeden stopien_celsjusza",
+            0: "zero stopni_celsjusza",
+            1: "jeden stopien_celsjusza",
+            2: "dwa stopnie_celsjusza",
+            20: "dwadziescia stopni_celsjusza",
+        }
+
+        for value, expected in test_cases.items():
+            self.assertEqual(expected, self.lang.read_temperature(value))
 
     def test_read_speed(self):
-        self.assertEqual(self.lang.read_speed(1, unit='mps'), "jeden metr_na_sekunde")
-        self.assertEqual(self.lang.read_speed(2, unit='mps'), "dwa metry_na_sekunde")
-        self.assertEqual(self.lang.read_speed(5, unit='mps'), "piec metrow_na_sekunde")
-        self.assertEqual(self.lang.read_speed(1, unit='kmph'), "jeden kilometr_na_godzine")
-        self.assertEqual(self.lang.read_speed(2, unit='kmph'), "dwa kilometry_na_godzine")
-        self.assertEqual(self.lang.read_speed(5, unit='kmph'), "piec kilometrow_na_godzine")
+        multiple_test_cases = {
+            'mps': {
+                1:  "jeden metr_na_sekunde",
+                2:  "dwa metry_na_sekunde",
+                5:  "piec metrow_na_sekunde",
+            },
+            'kmph': {
+                1: "jeden kilometr_na_godzine",
+                2: "dwa kilometry_na_godzine",
+                5: "piec kilometrow_na_godzine",
+            }
+        }
+
+        for unit, test_cases in multiple_test_cases.items():
+            for value, expected in test_cases.items():
+                self.assertEqual(expected,
+                                 self.lang.read_speed(value, unit=unit))
+
 
     def test_read_degrees(self):
-        self.assertEqual(self.lang.read_degrees(0), "zero stopni")
-        self.assertEqual(self.lang.read_degrees(1), "jeden stopien")
-        self.assertEqual(self.lang.read_degrees(2), "dwa stopnie")
-        self.assertEqual(self.lang.read_degrees(5), "piec stopni")
+        test_cases = {
+            0: "zero stopni",
+            1: "jeden stopien",
+            2: "dwa stopnie",
+            5: "piec stopni",
+        }
+
+        for value, expected in test_cases.items():
+            self.assertEqual(expected, self.lang.read_degrees(value))
 
     def test_read_direction(self):
-        testcases = {
-            'N':   ('polnocny',                       'polnocny',),
-            'NNE': ('polnocno-polnocno-wschodni',     'polnocno-wschodni',),
-            'NE':  ('polnocno-wschodni',              'polnocno-wschodni',),
-            'ENE': ('wschodnio-polnocno-wschodni',   'polnocno-wschodni',),
-            'E':   ('wschodni',                       'wschodni',),
-            'ESE': ('wschodnio-poludniowo-wschodni',  'poludniowo-wschodni',),
-            'SE':  ('poludniowo-wschodni',             'poludniowo-wschodni',),
-            'SSE': ('poludniowo-poludniowo-wschodni', 'poludniowo-wschodni',),
-            'S':   ('poludniowy',                     'poludniowy',),
-            'SSW': ('poludniowo-poludniowo-zachodni', 'poludniowo-zachodni',),
-            'SW':  ('poludniowo-zachodni',            'poludniowo-zachodni',),
-            'WSW': ('zachodnio-poludniowo-zachodni',  'poludniowo-zachodni',),
-            'W':   ('zachodni',                       'zachodni',),
-            'WNW': ('zachodnio-polnocno-zachodni',    'polnocno-zachodni',),
-            'NW':  ('polnocno-zachodni',              'polnocno-zachodni',),
-            'NNW': ('polnocno-polnocno-zachodni',     'polnocno-zachodni',),
+        test_cases = {
+            'N':   ('polnocny',
+                    'polnocny',),
+            'NNE': ('polnocno-polnocno-wschodni',
+                    'polnocno-wschodni',),
+            'NE':  ('polnocno-wschodni',
+                    'polnocno-wschodni',),
+            'ENE': ('wschodnio-polnocno-wschodni',
+                    'polnocno-wschodni',),
+            'E':   ('wschodni',
+                    'wschodni',),
+            'ESE': ('wschodnio-poludniowo-wschodni',
+                    'poludniowo-wschodni',),
+            'SE':  ('poludniowo-wschodni',
+                    'poludniowo-wschodni',),
+            'SSE': ('poludniowo-poludniowo-wschodni',
+                    'poludniowo-wschodni',),
+            'S':   ('poludniowy',
+                    'poludniowy',),
+            'SSW': ('poludniowo-poludniowo-zachodni',
+                    'poludniowo-zachodni',),
+            'SW':  ('poludniowo-zachodni',
+                    'poludniowo-zachodni',),
+            'WSW': ('zachodnio-poludniowo-zachodni',
+                    'poludniowo-zachodni',),
+            'W':   ('zachodni',
+                    'zachodni',),
+            'WNW': ('zachodnio-polnocno-zachodni',
+                    'polnocno-zachodni',),
+            'NW':  ('polnocno-zachodni',
+                    'polnocno-zachodni',),
+            'NNW': ('polnocno-polnocno-zachodni',
+                    'polnocno-zachodni',),
         }
-        for testcase in testcases.items():
-            test, result = testcase
-            self.assertEqual(self.lang.read_direction(test), result[0])
-            self.assertEqual(self.lang.read_direction(test, short=True), result[1])
+        for value, expected in test_cases.items():
+            self.assertEqual(expected[0], self.lang.read_direction(value))
+            self.assertEqual(expected[1],
+                             self.lang.read_direction(value, short=True))
 
     def test_read_datetime(self):
         in_fmt = '%Y-%m-%d %H:%M'
         out_fmt = '%d %B %H %M'
 
-        self.assertEqual(self.lang.read_datetime(datetime.datetime.strptime('1983-06-04 14:26', in_fmt), out_fmt='%d %B %H %M'),
-                         'czwartego czerwca czternasta dwadziescia szesc')
-        self.assertEqual(self.lang.read_datetime('2014-11-29 18:27', in_fmt=in_fmt, out_fmt='%d %B %H %M'),
-                         'dwudziestego dziewiatego listopada osiemnasta dwadziescia siedem')
-        self.assertEqual(self.lang.read_datetime('2014-05-03 0:00', in_fmt=in_fmt, out_fmt='%d %B godzina %H %M'),
-                         'trzeciego maja godzina zero zero-zero')
-        self.assertEqual(self.lang.read_datetime('2014-05-03 20:00', in_fmt=in_fmt,
-                         out_fmt=out_fmt),
-                         'trzeciego maja dwudziesta zero-zero')
-        self.assertEqual(self.lang.read_datetime('2014-05-03 21:05', in_fmt=in_fmt, out_fmt=out_fmt),
-                         'trzeciego maja dwudziesta pierwsza piec')
-        self.assertEqual(self.lang.read_datetime('2014-05-03 08:25 AM',
-                                                 in_fmt='%Y-%m-%d %I:%M %p',
-                                                 out_fmt=out_fmt),
-                         'trzeciego maja osma dwadziescia piec')
-        self.assertEqual(self.lang.read_datetime('2014-05-03 08:25 PM',
-                                                 in_fmt='%Y-%m-%d %I:%M %p',
-                                                 out_fmt=out_fmt),
-                         'trzeciego maja dwudziesta dwadziescia piec')
+        test_cases = {
+            'czwartego czerwca czternasta dwadziescia szesc': {
+                'dt': '1983-06-04 14:26',
+                'in_fmt': in_fmt,
+                'out_fmt': '%d %B %H %M',
+            },
+            'dwudziestego dziewiatego listopada osiemnasta dwadziescia siedem': {
+                'dt': '2014-11-29 18:27',
+                'in_fmt': in_fmt,
+                'out_fmt': '%d %B %H %M',
+            },
+            'trzeciego maja godzina zero zero-zero': {
+                'dt': '2014-05-03 0:00',
+                'in_fmt': in_fmt,
+                'out_fmt': '%d %B godzina %H %M',
+            },
+            'trzeciego maja dwudziesta zero-zero': {
+                'dt': '2014-05-03 20:00',
+                'in_fmt': in_fmt,
+                'out_fmt': out_fmt,
+            },
+
+            'trzeciego maja dwudziesta pierwsza piec': {
+                'dt': '2014-05-03 21:05',
+                'in_fmt': in_fmt,
+                'out_fmt': out_fmt,
+            },
+            'trzeciego maja osma dwadziescia piec': {
+                'dt': '2014-05-03 08:25 AM',
+                'in_fmt': '%Y-%m-%d %I:%M %p',
+                'out_fmt': out_fmt,
+            },
+            'trzeciego maja dwudziesta dwadziescia piec': {
+                'dt': '2014-05-03 08:25 PM',
+                'in_fmt': '%Y-%m-%d %I:%M %p',
+                'out_fmt': out_fmt,
+            },
+        }
+
+        for expected, args in test_cases.items():
+            dt = datetime.datetime.strptime(args['dt'], args['in_fmt'])
+            self.assertEqual(expected,
+                             self.lang.read_datetime(dt, out_fmt=args['out_fmt']))
+
         self.assertRaises(ValueError, self.lang.read_datetime,
                           *('2014-05-03 0:00', '%d %B %%', in_fmt)),
         self.assertRaises(TypeError, self.lang.read_datetime,
@@ -104,7 +180,9 @@ class TestKwotaSlownieFunctions(unittest.TestCase):
 
     def test_read_callsign(self):
         self.assertEqual("stefan quebec szesc jozef natalia xawery "
-                          "lamane pawel", self.lang.read_callsign('sq6jnx/p'))
+                         "lamane pawel", self.lang.read_callsign('sq6jnx/p'))
         self.assertRaises(ValueError, self.lang.read_callsign, *('sq?jnx',))
+
+
 if __name__ == '__main__':
     unittest.main()
